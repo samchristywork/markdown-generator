130237f381dfe00caf1f10ee2a0db44eb2dabcf4
samchristywork <106397573+samchristywork@users.noreply.github.com>
2023-04-13 19:31:24 -0500

    Feature: Sort list using custom comparator
    The list should be sorted by category. I may also do a secondary sort on
    the skill name to make it deterministic.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
-#include "yaml-cpp/include/yaml-cpp/yaml.h"
 #include <iostream>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <yaml-cpp/node/detail/iterator_fwd.h>
+#include <yaml-cpp/yaml.h>
 
 using namespace std;
 
+vector<YAML::const_iterator> elements;
+
 void printRow(YAML::const_iterator e) {
-  cout << "|" << e->first << "|"
+  cout << "|" << e->second["category"] << " - " << e->first << "|"
        << "<img src=\"https://s-christy.com/icons/" << e->second["lang"]
        << ".png\" height=20></img>"
        << "<a href=\"" << e->second["link"] << "\">Sample</a>"
        << "|" << e->second["description"] << "|" << endl;
+}
+
+bool compare_function(YAML::const_iterator a, YAML::const_iterator b) {
+  return a->second["category"].as<string>() <
+         b->second["category"].as<string>();
 }
 
 void usage(char *name) { cout << "Usage: " << name << " <file>" << endl; }
@@ -20,10 +31,17 @@ int main(int argc, char *argv[]) {
   }
 
   cout << "|Skill|Example|Description|" << endl;
+  cout << "|-----|-------|-----------|" << endl;
   YAML::Node config = YAML::LoadFile(argv[1]);
 
   YAML::Node skills = config["skills"];
   for (YAML::const_iterator e = skills.begin(); e != skills.end(); ++e) {
+    elements.push_back(e);
+  }
+
+  std::sort(elements.begin(), elements.end(), compare_function);
+
+  for (auto e : elements) {
     printRow(e);
   }
 }
